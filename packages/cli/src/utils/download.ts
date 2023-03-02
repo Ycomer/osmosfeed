c53dd846abe9b9fed88bb37188f22c4187d969c9
ycomer <dongfangyuan404@gmail.com>
2023-03-02 23:33:14 +0800

    fix: remove console

--- packages/cli/src/utils/download.ts
+++ packages/cli/src/utils/download.ts
@@ -32,7 +32,6 @@ async function downloadHelper(url: string, config: DownloadHelperConfig): Promis
 
   try {
     const response = await axios.get(url, { timeout: timeoutInMs });
-    console.log(response, "responseresponseresponseasduak");
     const responseText = response.data;
     if (response.status !== 200) {
       throw new Error(`[download] Error download ${url}, status ${response.status}`);
